4f2b6ffa0a19ab5aa1b55473af0e279963384fec
Efraim Yosofov <efraim224@gmail.com>
2023-05-28 22:50:23 +0300

    added server functionality to get posts, create

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,34 @@
 import PostPreview from "../components/PostPreview"
 import LinkContainer from '../components/LinkContainer';
 import { Link } from 'react-router-dom';
-import { posts } from "../content/posts";
+// import { posts } from "../content/posts";
+import { useEffect, useState } from "react";
+import axios from 'axios'
 
 const Links = [
     { title: "Popular posts", links: ["asdf", "asdf"] },
     { title: "Hot news", links: ["asdf", "asdf"] }
 ]
 
+const postsLink = "http://localhost:5000/posts"
+
 const Home = () => {
+
+    const [postsData, setpostsData] = useState([]);
+    const getData = async () => {
+        const { data } = await axios.get(postsLink);
+        setpostsData(data);
+    };
+
+    useEffect(() => {
+        try {
+            getData();
+        }
+        catch (e) {
+        }
+    }, []);
+
+
     return (
         <div className="home-container">
             <main>
@@ -18,15 +38,15 @@ const Home = () => {
                         <button>Create new post</button>
                     </Link>
                 </div>
-                {posts.map(item => {
+                {postsData.map(item => {
                     return <PostPreview {...item} />
                 })}
             </main>
             <right>
                 {Links.map(item => {
-                return <LinkContainer {...item} />
+                    return <LinkContainer {...item} />
                 })}
-          </right>
+            </right>
         </div>
     )
 }
--- src/pages/NewPost.js
+++ src/pages/NewPost.js
@@ -1,17 +1,37 @@
-
+import axios from "axios";
 
 const NewPost = () => {
 
+    const link = "http://localhost:5000/posts/"
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const title = event.target.form_title.value;
+        const content = event.target.form_content.value;
+        const fetchData = async () => {
+            try {
+                axios.post(link, {
+                    "title": title,
+                    "content": content
+                })
+            }
+            catch (e) {
+                console.log(e)
+            }
+        }
+
+        fetchData();
+    }
+
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     Title:
-                    <input type="text" name="title"/>
+                    <input type="text" id="form_title"/>
                 </label>
                 <label>
                     Content:
-                    <input type="textarea" name="content" rows={5}  cols={5}/>
+                    <input type="textarea" id="form_content" rows={5}  cols={5}/>
                 </label>
                 <input type="submit" value="Submit" />
             </form>
